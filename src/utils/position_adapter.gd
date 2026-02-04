8f6d445fbbc60a3298d89266474ddaf645cf546d
dvtzoe <ohmtzoe@gmail.com>
2026-02-04 21:47:07 +0700

    idfk

--- src/utils/position_adapter.gd
+++ src/utils/position_adapter.gd
@@ -4,6 +4,6 @@ const TILE_SIZE := Vector2i(110, 128)
 
 static func tile_to_px(coordinate: Vector2i) -> Vector2:
     if coordinate.y % 2 == 0:
-        return Vector2(coordinate.x * TILE_SIZE.x, coordinate.y * TILE_SIZE.y * 3.0 / 4)
+        return Vector2(coordinate.x * TILE_SIZE.x + TILE_SIZE.x * 0.5, coordinate.y * TILE_SIZE.y * 0.75 + TILE_SIZE.y * 0.5)
     else:
-        return Vector2(coordinate.x * TILE_SIZE.x + TILE_SIZE.x / 2.0, coordinate.y * TILE_SIZE.y * 3.0 / 4)
+        return Vector2(coordinate.x * TILE_SIZE.x + TILE_SIZE.x, coordinate.y * TILE_SIZE.y * 0.75 + TILE_SIZE.y * 0.5)
