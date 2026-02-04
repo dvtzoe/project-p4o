aeae97c5d6352970e8a584ffba5448e1bd9aa71f
dvtzoe <ohmtzoe@gmail.com>
2026-02-04 18:43:04 +0700

    change: redefine unit

--- src/game/stage/units/unit.gd
+++ src/game/stage/units/unit.gd
@@ -1,11 +1,4 @@
 extends Node2D
 
-@export var unit_name: String
-@export var max_health: int
-
+var unit_type: String
 var id: int
-var hp: int
-var grid_pos: Vector2i
-
-func _ready():
-    hp = max_health
