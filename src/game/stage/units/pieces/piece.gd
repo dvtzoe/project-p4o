e7b6d6576a65878b698679fdc101b962f2ec9590
dvtzoe <ohmtzoe@gmail.com>
2026-02-04 18:48:25 +0700

    feat: simple piece

---
+++ src/game/stage/units/pieces/piece.gd
@@ -0,0 +1,4 @@
+extends "res://src/game/stage/units/unit.gd"
+
+func move_to(target: Vector2i) -> void:
+    coordinate = target
--- src/game/stage/units/unit.gd
+++ src/game/stage/units/unit.gd
@@ -2,3 +2,5 @@ extends Node2D
 
 var unit_type: String
 var id: int
+
+var coordinate: Vector2i
