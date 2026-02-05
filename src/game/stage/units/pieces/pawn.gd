e14879d21b0fa91939359df9c982aecd43bcf926
dvtzoe <ohmtzoe@gmail.com>
2026-02-05 12:28:42 +0700

    feat: dijkstra movement

--- src/game/stage/units/pieces/pawn.gd
+++ src/game/stage/units/pieces/pawn.gd
@@ -2,13 +2,7 @@ extends "res://src/game/stage/units/pieces/piece.gd"
 
 func compute_reachable_tiles() -> void:
     reachable_tiles.clear()
-    var adjacent_hexes = HexUtils.get_adjacent_hex(coord)
-    for hex in adjacent_hexes:
-        if hex in StageState.instance.unit_at:
-            continue
-        if StageState.instance.tile_map_layer.get_cell_source_id(hex) == -1:
-            continue
-        reachable_tiles[hex] = 1
+    reachable_tiles = Movements.dijkstra_reachable_tiles(coord, 3)
 
 func _ready() -> void:
     type_name = "Pawn"
--- src/utils/constants.gd
+++ src/utils/constants.gd
@@ -4,12 +4,9 @@ const UNITS_TABLE = {
     "base": "res://src/game/stage/units/structures/base.tscn",
     "pawn": "res://src/game/stage/units/pieces/pawn.tscn",
 }
-
-var tiles: Dictionary
-
-func _init() -> void:
-    var file = FileAccess.open("res://src/game/stage/tiles.json", FileAccess.READ)
-    if file:
-        var json_content: String = file.get_as_text()
-        tiles = JSON.parse_string(json_content).result
-    file.close()
+const TILES = {
+    0: {
+        "name": "Hexagon",
+        "movement_cost": 1
+    }
+}
