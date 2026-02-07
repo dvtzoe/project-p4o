df3649929eaec99241d544547ac5b9405a5cfafa
dvtzoe <ohmtzoe@gmail.com>
2026-02-07 15:27:28 +0700

    feat: show team in unit status

--- src/game/stage/unit_status/unit_status.gd
+++ src/game/stage/unit_status/unit_status.gd
@@ -3,10 +3,15 @@ extends Control
 class_name UnitStatus
 
 @export var name_label: Label
+@export var team_label: Label
 @export var actions_container: HBoxContainer
 
 func show_info(unit: Unit) -> void:
     name_label.text = unit.unit_name
+    if unit.team == "player":
+        team_label.text = ""
+    else:
+        team_label.text = "(%s)" % unit.team
 
     var action_card_scene := preload("res://src/game/stage/unit_status/action_card.tscn")
     for action: Action in unit.actions:
