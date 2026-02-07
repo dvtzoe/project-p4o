4e3b7ad5b64a8e0b01abf5a8cdae8e64efb6b4c0
dvtzoe <ohmtzoe@gmail.com>
2026-02-07 10:51:06 +0700

    fix: unit_status to use the new unit name

--- src/game/stage/unit_status/unit_status.gd
+++ src/game/stage/unit_status/unit_status.gd
@@ -5,11 +5,11 @@ class_name UnitStatus
 @export var name_label: Label
 @export var actions_container: HBoxContainer
 
-func show_info(unit: Node2D) -> void:
-    name_label.text = unit.get("type_name")
+func show_info(unit: Unit) -> void:
+    name_label.text = unit.unit_name
 
     var action_card_scene := preload("res://src/game/stage/unit_status/action_card.tscn")
-    for action: Action in unit.get_node("Actions").get_children():
+    for action: Action in unit.actions:
         var action_card_instance := action_card_scene.instantiate()
         action_card_instance.call("setup", action)
         actions_container.add_child(action_card_instance)
