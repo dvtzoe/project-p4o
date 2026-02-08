a82e3ff98937c2478fb6e32386d461e5bc850437
dvtzoe <ohmtzoe@gmail.com>
2026-02-08 16:15:17 +0700

    fix: change hard coded path

--- src/game/lobby/lobby.gd
+++ src/game/lobby/lobby.gd
@@ -4,4 +4,4 @@ class_name Lobby
 
 func _on_play_button_pressed() -> void:
     Game.change_state(Enums.GameState.STAGE)
-    Game.stage.start(load("res://assets/stages/intro.tres") as StageResource)
+    Game.stage.start(load("res://data/stages/intro.tres") as StageResource)
