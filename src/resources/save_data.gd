6fefef77946925cde7707e9374261fd5d078a68d
dvtzoe <ohmtzoe@gmail.com>
2026-02-09 18:08:14 +0700

    idfk: add time in SaveData

--- src/resources/save_data.gd
+++ src/resources/save_data.gd
@@ -2,7 +2,14 @@ extends Resource
 
 class_name SaveData
 
+## Days passed
 var day: int = 0
+
+## Current time (0-23)
+var time: int = 8
+
+## Current route
 var route: String = "default"
 
+## Datables owned by the player
 var datables: Dictionary[String, Datable] = {}
