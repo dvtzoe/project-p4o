4bc4abdcdd813d337967d059e79d6b0fd97e7323
dvtzoe <ohmtzoe@gmail.com>
2026-02-12 20:29:51 +0700

    feat: stage summary that also return to place

---
+++ src/game/stage/summary/summary.gd
@@ -0,0 +1,15 @@
+extends Control
+
+class_name StageSummary
+
+@export var return_button: Button
+
+func _on_return_button_pressed() -> void:
+    Game.change_state("place")
+
+func _on_all_objectives_completed() -> void:
+    visible = true
+
+func _ready() -> void:
+    return_button.pressed.connect(_on_return_button_pressed)
+    Game.stage.objective.all_objectives_completed.connect(_on_all_objectives_completed)
