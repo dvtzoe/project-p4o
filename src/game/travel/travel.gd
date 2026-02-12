ee0b1b808fdf32046e20de6a029921f65e5f12c1
dvtzoe <ohmtzoe@gmail.com>
2026-02-12 22:55:14 +0700

    refactor(travel)

--- src/game/travel/travel.gd
+++ src/game/travel/travel.gd
@@ -2,19 +2,18 @@ extends Control
 
 class_name Travel
 
-@export var TravelEntryContainer: VBoxContainer
+const TRAVEL_ENTRY_SCENE := preload("res://src/game/travel/travel_entry.tscn")
+
+@export var travel_entry_container: VBoxContainer
 
 func _on_explore_pressed() -> void:
     Game.change_state(Game.States.EXPLORE)
     queue_free()
 
 func _ready() -> void:
-    var travel_entry_scene = preload("res://src/game/travel/travel_entry.tscn")
-    
-    var explore_entry: TravelEntry = travel_entry_scene.instantiate()
-    explore_entry.setup("Explore")
-    explore_entry.connect("pressed", Callable(self , "_on_explore_pressed"))
-    TravelEntryContainer.add_child(explore_entry)
+    var explore_entry := TRAVEL_ENTRY_SCENE.instantiate() as TravelEntry
+    explore_entry.setup("Explore", _on_explore_pressed)
+    travel_entry_container.add_child(explore_entry)
 
 func _on_back_button_pressed() -> void:
     Game.change_state(Game.States.PLACE)
--- src/game/travel/travel_entry.gd
+++ src/game/travel/travel_entry.gd
@@ -4,5 +4,6 @@ class_name TravelEntry
 
 @export var label: Label
 
-func setup(entry_name: String) -> void:
+func setup(entry_name: String, on_press: Callable) -> void:
     label.text = entry_name
+    pressed.connect(on_press)
