0eb91f98ed4cd6e328b805a9c91d2a6f01cd9dfe
dvtzoe <ohmtzoe@gmail.com>
2026-02-24 08:33:33 +0700

    fix: add TILES indexing

--- src/utils/constants.gd
+++ src/utils/constants.gd
@@ -8,5 +8,22 @@ const TILES = {
     0: {
         "name": "Hexagon",
         "movement_cost": 1
-    }
+    },
+    1: {},
+    2: {
+        "name": "Dirt",
+        "movement_cost": 1
+    },
+    3: {
+        "name": "Grass",
+        "movement_cost": 1
+    },
+    4: {
+        "name": "Grass",
+        "movement_cost": 1
+    },
+    5: {
+        "name": "Grass",
+        "movement_cost": 1
+    },
 }
